2c11998fc563944d63da631adf7e7ead956d5281
akiroaki <56996146+akiroaki@users.noreply.github.com>
2023-01-29 10:53:41 +0100

    basic functionalities test

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var lab1_1 = require("./lab/lab1_1").lab
-var lab1_2 = require("./lab/lab1_2").lab;
+// var lab1_2 = require("./lab/lab1_2").lab;
 
 var PORT = 8080;
 
 
 var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
-	{path: "/digest", action: lab1_1},	
-	{path: "/instance", action: lab1_2}, 
+	{path: "/digest", action: lab1_1}//,
+	// {path: "/instance", action: lab1_2}, 
 	];
 
 var service = require("./lib/service").http(urlMap);
--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -1,34 +1,34 @@
-//stub for lab 1_2
-var AWS = require('aws-sdk');
-let os = require("os");
-AWS.config.loadFromPath('./config.json');
+// //stub for lab 1_2
+// var AWS = require('aws-sdk');
+// let os = require("os");
+// AWS.config.loadFromPath('./config.json');
 
-let ec2 = new AWS.EC2({region: 'us-east-1'});
+// let ec2 = new AWS.EC2({region: 'us-east-1'});
 
-var task =  function(request, callback){
-	var params = {
-        ImageId: "ami-0574da719dca65348",  
-        InstanceType: "t2.micro",
-        MaxCount: 1,
-        MinCount: 1,
-       };
+// var task =  function(request, callback){
+// 	var params = {
+//         ImageId: "ami-0574da719dca65348",  
+//         InstanceType: "t2.micro",
+//         MaxCount: 1,
+//         MinCount: 1,
+//        };
        
-       ec2.runInstances(params, function(err, data) {
-         if (err) console.log(err, err.stack); // an error occurred
-         else{
-            console.log(data);           // successful response
+//        ec2.runInstances(params, function(err, data) {
+//          if (err) console.log(err, err.stack); // an error occurred
+//          else{
+//             console.log(data);           // successful response
 
-            let params = { InstanceIds: data.Instances.map(instance => instance.InstanceId) };
-            ec2.waitFor('instanceRunning', params, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                else{     
-                    console.log(data);           // successful response
-                    const instancesPublicIds = data.Reservations[0].Instances.map(instance => instance.PublicIpAddress).filter(id => id);
-                    callback(null, 'IP/DNS of ran instance:<br>' + instancesPublicIds.join('<br>') + `<hr><br>Service provided by: ${os.hostname()}`);
-                }
-          });
-       }
-    });
-}
+//             let params = { InstanceIds: data.Instances.map(instance => instance.InstanceId) };
+//             ec2.waitFor('instanceRunning', params, function(err, data) {
+//                 if (err) console.log(err, err.stack); // an error occurred
+//                 else{     
+//                     console.log(data);           // successful response
+//                     const instancesPublicIds = data.Reservations[0].Instances.map(instance => instance.PublicIpAddress).filter(id => id);
+//                     callback(null, 'IP/DNS of ran instance:<br>' + instancesPublicIds.join('<br>') + `<hr><br>Service provided by: ${os.hostname()}`);
+//                 }
+//           });
+//        }
+//     });
+// }
 
-exports.lab = task
+// exports.lab = task
